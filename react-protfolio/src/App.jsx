3ea1f232984996168143a48eb83ff7125ae01f2a
Mehak Sadique <mksd1999@gmail.com>
2025-02-07 22:35:10 -0500

    set up basic protfolio

--- react-protfolio/src/App.jsx
+++ react-protfolio/src/App.jsx
@@ -2,12 +2,23 @@
 
 import styles from './App.module.css'
 import { Navbar } from "./components/Navbar/Navbar";
-
+import { Hero } from "./components/Hero/Hero";
+import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/experience";
+import { Projects } from "./components/Projects/Projects";
+import { Contact } from "./components/Contact/Contact";
 function App() {
 
 
   return <div className={styles.App}>
     <Navbar />
+
+    <Hero/>
+   
+    <About/>
+    <Experience/>
+    <Projects/>
+    <Contact/>
      </div>;
   
 }
