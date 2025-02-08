5db48256ef3645c8ff6f92c084f4442cf3b45795
Mehak Sadique <mksd1999@gmail.com>
2025-02-08 08:58:29 -0500

    updates

--- react-protfolio/src/components/Hero/Hero.jsx
+++ react-protfolio/src/components/Hero/Hero.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
+import Typical from "react-typical";
 
 export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+      <h1 className={styles.title}>
+  <Typical steps={["Hi, I'm Mehak Sadique", 2000]} wrapper="span" />
+</h1>
         <p className={styles.description}>
           I'm a full-stack developer with 5 years of experience using React and
           NodeJS. Reach out if you'd like to learn more!
